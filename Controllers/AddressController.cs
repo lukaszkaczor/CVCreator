279457fd2cc841b906b833995d6cfba58ca44c3a
sf <s@s.com>
2021-10-24 17:49:06 +0200

    Update AddressController

--- Controllers/AddressController.cs
+++ Controllers/AddressController.cs
@@ -1,14 +1,15 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using CvCreator.Data;
 using CvCreator.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CvCreator.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
+    // [Authorize]
     public class AddressController : ControllerBase
     {
         private ApplicationDbContext _context;
@@ -19,17 +20,54 @@ namespace CvCreator.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Address> Get()
+        public async Task<IEnumerable<Address>> Get() => await _context.Addresses.ToListAsync();
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Address>> Get(int id)
         {
-            var list = _context.Addresses.ToList();
-            return list;
+            var address = await _context.Addresses.FindAsync(id);
+
+            if (address is null) return NotFound();
+
+            return address;
         }
 
         [HttpPost]
-        public async Task Post(Address address)
+        public async Task<ActionResult<Address>> Post(Address todoItem)
         {
-            var ss = _context.Addresses.Add(address);
+            _context.Addresses.Add(todoItem);
             await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get), new { id = todoItem.Id }, todoItem);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Address address)
+        {
+            if (id != address.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(address).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (_context.Addresses.Find(id) is null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
         [HttpDelete("{id}")]
