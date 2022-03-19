978c7cc08947c8b4f70dc597010c87517ed22735
sf <s@s.com>
2022-03-19 18:05:53 +0100

    Test

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Claims;
+using System.Text;
+using System.Text.RegularExpressions;
+using CVCreator.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CVCreator.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost, Route("login")]
+        public IActionResult Login([FromBody] LogisModel user)
+        {
+            if (user == null)
+                return BadRequest();
+
+            if (user.Username == "asd" && user.Password == "asd")
+            {
+                var secretkey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("securitykey"));
+                var signingCreditentials = new SigningCredentials(secretkey, SecurityAlgorithms.HmacSha256);
+
+                var tokenOptions = new JwtSecurityToken(
+                    issuer: "https://https://localhost:5001/",
+                    audience: "https://https://localhost:5001/",
+                    claims: new List<Claim>(),
+                    expires: DateTime.Now.AddMinutes(60),
+                    signingCredentials: signingCreditentials
+                );
+
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+
+                return Ok(new { Token = tokenString });
+            }
+
+            return Unauthorized();
+        }
+    }
+}
---
+++ Models/LogisModel.cs
@@ -0,0 +1,8 @@
+namespace CVCreator.Models
+{
+    public class LogisModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
